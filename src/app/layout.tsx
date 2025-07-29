fd42cb0c4ab14a3f03fcc2320f0de2979982a71e
Yongyong Wu <104568477+wyy000111@users.noreply.github.com>
2025-07-29 14:13:18 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -90,6 +90,31 @@ export default async function RootLayout({
           </SiteProvider>
         </ThemeProvider>
       </body>
+ <div  class="text-center">
+    Powered by 💝💝💝
+<a href="https://cloudflare.com/" rel="noopener noreferrer" target="_blank">赛博菩萨</a>；
+  <a href="https://github.com/" rel="noopener noreferrer" target="_blank">小黄人</a>；
+  <a href="https://www.cloudns.net/" rel="noopener noreferrer" target="_blank">CloudNS</a>；
+  <a href="https://account.proton.me/mail" rel="noopener noreferrer" target="_blank">Proton Mail</a>；💝💝💝
+</div>
+<div  class="text-center">
+   <p>
+   🛠️🛠️🛠️网站联盟（自用）：
+   <a href="https://imgbed.19781126.xyz/" rel="noopener noreferrer" target="_blank">图床</a>；
+    <a href="https://paste.19781126.xyz/" rel="noopener noreferrer" target="_blank">网盘/WebDav</a>；
+    <a href="https://tv.19781126.xyz/" rel="noopener noreferrer" target="_blank">在线TV</a>；
+    <a href="https://tempemail.19781126.xyz/" rel="noopener noreferrer" target="_blank">临时邮箱</a>；
+    <a href="https://media.19781126.xyz/" rel="noopener noreferrer" target="_blank">多媒体博客</a>；
+    <a href="https://github.19781126.xyz/" rel="noopener noreferrer" target="_blank">GH加速</a>；
+    <a href="https://comment.19781126.xyz/" rel="noopener noreferrer" target="_blank">评论</a>；
+    <a href="https://mail.19781126.xyz/" rel="noopener noreferrer" target="_blank">邮箱</a>；
+    <a href="https://chat.19781126.xyz/" rel="noopener noreferrer" target="_blank"> AI </a>；
+    <a href="https://www.19781126.xyz/" rel="noopener noreferrer" target="_blank">博客</a>；
+    <a href="https://epush.19781126.xyz/" rel="noopener noreferrer" target="_blank">消息推送</a>；
+         <a href="https://moontv.19781126.xyz/" rel="noopener noreferrer" target="_blank">MoonTV</a>；🛠️🛠️🛠️
+ </p>
+ <hr />
+</div>
     </html>
   );
 }
